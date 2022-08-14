d9cc0e0a6fa7b8e44349383f7aee4fece0719110
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 16:13:57 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,17 @@
 import React from 'react';
 const Todo = () => {
     return (
-        <></>
+        <>
+        <h2 class="todo">Todo</h2>
+        {
+            state.map((cur)=>
+            <>
+            <div class="todo-title" key={id}>{cur}</div>
+            <button onClick={()=>dispatch({type:"delete"})}>del</button>
+            </>
+        }
+       
+        </>
     )
 }
 
