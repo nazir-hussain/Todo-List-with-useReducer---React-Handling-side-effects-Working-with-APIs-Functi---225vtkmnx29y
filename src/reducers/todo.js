4aaae1727436be281c376949f5da46157ba2313f
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-14 16:15:18 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,6 +1,13 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
-
+        case "Add":
+            return{
+                state:[...state,{text : action.text}]
+            }
+        case "Delete":
+            return{
+                state: state.filter((cur,ind)=>ind!==id)
+            }
     }
 }
 
