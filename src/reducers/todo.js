3824359177fee0fc068a8b2a019d5b357ce81557
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-27 00:35:42 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -17,31 +17,65 @@
 
 
 
-const todoReducer = (state, action) => {
-  switch (action.type) {
-    case "ADD": {
-      return {
-        todos: [
-          ...state.todos,
-          {
-            title: action.value,
-            id:
-              new Date().toLocaleDateString() +
-              "" +
-              new Date().toLocaleTimeString(),
-          },
-        ],
-      };
-    }
-    case "DELETE": {
-      return {
-        todos: action.data,
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-};
+// const todoReducer = (state, action) => {
+//   switch (action.type) {
+//     case "ADD": {
+//       return {
+//         todos: [
+//           ...state.todos,
+//           {
+//             title: action.value,
+//             id:
+//               new Date().toLocaleDateString() +
+//               "" +
+//               new Date().toLocaleTimeString(),
+//           },
+//         ],
+//       };
+//     }
+//     case "DELETE": {
+//       return {
+//         todos: action.data,
+//       };
+//     }
+//     default: {
+//       return state;
+//     }
+//   }
+// };
 
-export { todoReducer };
+// export { todoReducer };
+
+
+
+
+
+
+
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        case 'addTodo': {
+            
+            return [...state,{id: Date.now(), todo_name: action.payload, isDone: false}];
+        }
+        case 'done': {
+            return state.map((todo)=>{
+                if(todo.id===action.payload){
+                    return {...todo , isDone : !todo.isDone}
+                }else{
+                    return {...todo}
+                }
+            })
+        }
+        case 'delete': {
+            console.log(state);
+            return state.filter((todo)=>todo.id != action.payload);
+        }
+    }
+}
+export const ACTION = {
+    ADD_TODO : 'addTodo',
+    DONE : 'done',
+    DELETE : 'delete'
+}
+export { todoReducer }
