8eb1de0a9ec06790e0dd1e48ea036e6953648f3c
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-27 00:19:34 +0530

    Update Todo.js

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,38 @@
-import React from 'react';
-const Todo = () => {
-    return (
-        <>
-        <h2 class="todo">Todo</h2>
-        {
-            state.map((cur)=>
-            <>
-            <div class="todo-title" key={id}>{cur}</div>
-            <button onClick={()=>dispatch({type:"delete"})}>del</button>
-            </>
-        }
+// import React from 'react';
+// const Todo = () => {
+//     return (
+//         <>
+//         <h2 class="todo">Todo</h2>
+//         {
+//             state.map((cur)=>
+//             <>
+//             <div class="todo-title" key={id}>{cur}</div>
+//             <button onClick={()=>dispatch({type:"delete"})}>del</button>
+//             </>
+//         }
        
-        </>
-    )
-}
+//         </>
+//     )
+// }
 
 
-export { Todo }
+// export { Todo }
 
+
+
+
+
+import React from "react";
+import "../styles/App.css";
+const Todo = ({ title, id, handleDelete }) => {
+  return (
+    <div className="todo">
+      <div className="todo-title">{title}</div>
+      <button className="todo-delete" onClick={() => handleDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export { Todo };
