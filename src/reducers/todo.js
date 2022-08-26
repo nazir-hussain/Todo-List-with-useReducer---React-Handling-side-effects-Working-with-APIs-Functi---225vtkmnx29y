958d8036f0c78133815922912e40a3e7fa11626a
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-27 00:20:29 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,14 +1,47 @@
-const todoReducer = (state, action) => {
-    switch (action.type) {
-        case "Add":
-            return{
-                state:[...state,{text : action.text}]
-            }
-        case "Delete":
-            return{
-                state: state.filter((cur,ind)=>ind!==id)
-            }
-    }
-}
+// const todoReducer = (state, action) => {
+//     switch (action.type) {
+//         case "Add":
+//             return{
+//                 state:[...state,{text : action.text}]
+//             }
+//         case "Delete":
+//             return{
+//                 state: state.filter((cur,ind)=>ind!==id)
+//             }
+//     }
+// }
 
-export { todoReducer }
+// export { todoReducer }
+
+
+
+
+
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD": {
+      return {
+        todos: [
+          ...state.todos,
+          {
+            title: action.value,
+            id:
+              new Date().toLocaleDateString() +
+              "" +
+              new Date().toLocaleTimeString(),
+          },
+        ],
+      };
+    }
+    case "DELETE": {
+      return {
+        todos: action.data,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export { todoReducer };
