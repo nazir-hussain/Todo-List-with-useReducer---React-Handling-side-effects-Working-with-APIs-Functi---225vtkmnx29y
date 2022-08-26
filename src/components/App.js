a4354fdc41ccd19285440d40fac3876cd2a9dbe2
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2022-08-27 00:32:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -22,23 +22,47 @@
 
 
 
-import React, { useReducer } from "react";
-import { todoReducer } from "../reducers/todo";
-import "../styles/App.css";
-import { AddTodo } from "./AddTodo";
-import Navbar from "./Navbar";
-import { Todo } from "./Todo";
+// import React, { useReducer } from "react";
+// import { todoReducer } from "../reducers/todo";
+// import "../styles/App.css";
+// import { AddTodo } from "./AddTodo";
+// import Navbar from "./Navbar";
+// import { Todo } from "./Todo";
+// const App = () => {
+//   const [state, dispatch] = useReducer(todoReducer, {
+//     /* Initial todo state array, could be an array for storing indivial todo objects */
+//     todos: [],
+//   });
+//   return (
+//     <div id="main">
+//       <Navbar />
+//       <AddTodo state={state} dispatch={dispatch} />
+//       {/* Render list of Todo Components here */}
+//     </div>
+//   );
+// };
+// export default App;
+
+
+
+
+
+import React, { useReducer } from 'react'
+import { todoReducer, ACTION } from '../reducers/todo';
+import '../styles/App.css';
+import { AddTodo } from './AddTodo';
+import { Todo } from './Todo';
 const App = () => {
-  const [state, dispatch] = useReducer(todoReducer, {
-    /* Initial todo state array, could be an array for storing indivial todo objects */
-    todos: [],
-  });
+  const [state,dispatch] = useReducer(todoReducer,[])
+  
+ 
   return (
     <div id="main">
-      <Navbar />
-      <AddTodo state={state} dispatch={dispatch} />
-      {/* Render list of Todo Components here */}
+      <AddTodo dispatch={dispatch}/>
+      <Todo data={state} handleBtn={dispatch} />  
     </div>
-  );
-};
+  )
+}
+
+
 export default App;
